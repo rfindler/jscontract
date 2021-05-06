4c11b04405ce4fa8dc52e6cc3530f5634fe9cb05
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-05-05 19:06:43 -0500

    Minor improvements to run.bash

--- workspaces/type-explorer/run.bash
+++ workspaces/type-explorer/run.bash
@@ -2,6 +2,12 @@
 
 DIR="$(dirname "${BASH_SOURCE[0]}")"
 
+if [ ! -d "$DIR/DefinitelyTyped" ]; then
+  git clone https://github.com/DefinitelyTyped/DefinitelyTyped "$DIR/DefinitelyTyped"
+fi
+
+mkdir -p results
+
 for REL_PATH in "$PWD"/DefinitelyTyped/types/*; do
   BASENAME="$(basename "$REL_PATH")"
   printf "Testing %s...\n" "$BASENAME";
