047bf6e98c46328224f6aa395d5553f06a26fdb9
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-05-05 19:01:47 -0500

    Adds sorting and filtering to find good packages to try

--- workspaces/type-explorer/run.bash
+++ workspaces/type-explorer/run.bash
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir -p results
-
 DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 for REL_PATH in "$PWD"/DefinitelyTyped/types/*; do
   BASENAME="$(basename "$REL_PATH")"
   printf "Testing %s...\n" "$BASENAME";
   timeout 600 "$DIR"/bin/ct "$BASENAME" &> results/"$BASENAME"
+  rm -rf "$DIR"/sandbox
 done
