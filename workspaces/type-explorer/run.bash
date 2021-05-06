44bc0875a0489901e7cbef5c901c699e364a4252
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-05-05 19:07:04 -0500

    Reduce timeout in run.bash

--- workspaces/type-explorer/run.bash
+++ workspaces/type-explorer/run.bash
@@ -11,6 +11,6 @@ mkdir -p results
 for REL_PATH in "$PWD"/DefinitelyTyped/types/*; do
   BASENAME="$(basename "$REL_PATH")"
   printf "Testing %s...\n" "$BASENAME";
-  timeout 600 "$DIR"/bin/ct "$BASENAME" &> results/"$BASENAME"
+  timeout 150 "$DIR"/bin/ct "$BASENAME" &> results/"$BASENAME"
   rm -rf "$DIR"/sandbox
 done
